a0fe3ed21b6a35110a440253644f2e465c1cee04
Chris <chris.anthony.yee@gmail.com>
2019-08-20 16:56:23 -0700

    Address Book

---
+++ spec/menu_controller_spec.js
@@ -0,0 +1,9 @@
+const MenuController = require("../controllers/MenuController");
+const Contact = require("../db/models").Contact;
+describe("MenuController", () => {
+  beforeEach(() => {
+    this.menu = new MenuController();
+  });
+
+
+ });
